e59642e6e3b6a9ce657334076476f2b560984539
Muhammadsodiq <muhammadsodiqxabibulloxoji@gmail.com>
2022-08-04 15:06:14 +0500

    done

--- src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
+++ src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
@@ -2,6 +2,7 @@ package school.mjc.stage0.conditions.task2;
 
 public class CoinFlip {
     public void throwCoin(int from1UpTo1000) {
-
+        if (from1UpTo1000 >= 500) System.out.println("Tail");
+        else System.out.println("Eagle");
     }
 }
